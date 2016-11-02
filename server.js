e89224d4d1a27c03416495066e111d75b385beae
Aaron Meaney <aaronmeaney@hotmail.com>
2016-11-02 11:30:11 +0000

    Refactored server initialisation code.

--- server.js
+++ server.js
@@ -55,56 +55,60 @@ app.get("/api/test/checkAuthKey", function(req, res) {
 });
 
 /*
- * Server Initialisation Starts here
+ * Server Initialisation Code
 */
 
 //Connect to the database and then the server
-database.connect(function(err){
-   if (err){
-      console.log("Error connecting to database: " + err);
-      console.log("Stopping server");
-      process.exit(0);
-   }
-   else
-   {
-      console.log("Connected to database");
-      
-      //Create server key if it does not exist
-      database.query('SELECT * FROM server_information WHERE var_key="secret_key";', function (err, rows, fields){
-         if(err) { 
-            console.log("Database query error: " + err);
-            console.log("Stopping server");
-            process.exit(0);
-         };
+function initServer() {
+   database.connect(function(err){
+      if (err){
+         console.log("Error connecting to database: " + err);
+         console.log("Stopping server");
+         process.exit(0);
+      }
+      else
+      {
+         console.log("Connected to database");
          
-         if(rows.length == 0) {
-            //Key does not exist in DB
-            console.log("Server secret key does not exist. Adding to database.");
-            database.query('INSERT INTO server_information (var_key, var_value) VALUES ("secret_key", "' + secretKey + '");', function (err, rows, fields){
-               if(err) { 
-                  console.log("Database query error: " + err);
-                  console.log("Stopping server");
-                  process.exit(0);
-               };
-               
-               console.log("Secret Key added to database");
+         //Create server key if it does not exist
+         database.query('SELECT * FROM server_information WHERE var_key="secret_key";', function (err, rows, fields){
+            if(err) { 
+               console.log("Database query error: " + err);
+               console.log("Stopping server");
+               process.exit(0);
+            };
+            
+            if(rows.length == 0) {
+               //Key does not exist in DB
+               console.log("Server secret key does not exist. Adding to database.");
+               database.query('INSERT INTO server_information (var_key, var_value) VALUES ("secret_key", "' + secretKey + '");', function (err, rows, fields){
+                  if(err) { 
+                     console.log("Database query error: " + err);
+                     console.log("Stopping server");
+                     process.exit(0);
+                  };
+                  
+                  console.log("Secret Key added to database");
+                  startServer();
+               });
+            }
+            else {
+               //Key exists in DB
+               secretKey = rows[0].var_value;
                startServer();
-            });
-         }
-         else {
-            //Key exists in DB
-            secretKey = rows[0].var_value;
-            startServer();
-         }
+            }
+         });
+      }
+   });
+   //Start Server
+   function startServer() {
+      server = app.listen(8081, function () {
+         var host = server.address().address;
+         var port = server.address().port;
+         console.log("HuddlOut server is now listening at port " + port);
       });
    }
-});
-
-//Start Server
-function startServer() {
-   server = app.listen(8081, function () {
-      var host = server.address().address;
-      var port = server.address().port;
-      console.log("HuddlOut server is now listening at port " + port);
-   });
 }
+
+//Init the server
+initServer();
